43368834a9e499d38998cec2512e23fe0059148d
Yves <cauayves5@gmail.com>
2023-04-13 22:55:46 -0300

    requirement: participants route

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from "express"
+import cors from "cors"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+import dayjs from "dayjs"
+
+const app = express()
+const data = dayjs()
+const hour = data.format('HH:mm:ss')
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+
+let db
+let participants
+let messages
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => {
+        db = mongoClient.db()
+        participants = db.collection("participants")
+        messages = db.collection("messages")
+    })
+    .catch(() => console.log(err.message))
+
+app.post("/participants", (req, res) => {
+
+    const name = req.body.name
+
+    participants.insertOne({
+        nome: name,
+        lastStatus: Date.now()
+    })
+
+    messages.insertOne(
+        {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: hour
+        }
+    )
+    res.sendStatus(201)
+})
+
+
+const PORT = 5000
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
